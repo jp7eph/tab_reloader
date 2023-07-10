f186fe3644a89d50d0cdb237dd3699507390087c
Naoki Horie <jp7eph@gmail.com>
2023-07-10 22:36:27 +0900

    feat/1st release (#1)

---
+++ src/background.ts
@@ -0,0 +1,65 @@
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+    console.log({ request });
+
+    if (request.enable) {
+        createAlarm(request.id, request.interval);
+    } else {
+        clearAlarm(request.id)
+    }
+
+    sendResponse({ result: "create success" });
+})
+
+function createAlarm(id: number, interval: number) {
+    chrome.alarms.create(id.toString(), { periodInMinutes: interval })
+}
+
+function clearAlarm(id: number) {
+    chrome.alarms.clear(id.toString())
+}
+
+// alarms発火時のCallback
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log('[reload]' + alarm.name + ' ' + Date());
+    chrome.tabs.get(Number(alarm.name), function (tab) {
+        // 更新対象のタブが存在しない場合はAlarmから削除
+        if (tab == undefined) {
+            clearAlarm(Number(alarm.name));
+        }
+    });
+    chrome.tabs.reload(Number(alarm.name));
+});
+
+// タブ切替時にバッジ設定
+// TODO: App.vueの部分といい感じに統合したい
+chrome.tabs.onActivated.addListener(function (_tabId) {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, async function (tab) {
+        let text = "";
+        if (tab[0].id != undefined) {
+            await chrome.alarms.get(tab[0].id.toString(), function (alarm) {
+                if (alarm != undefined) {
+                    text = typeof alarm.periodInMinutes === "number" ? alarm.periodInMinutes.toString() : ""
+                }
+            });
+            // TODO: 共通化したい
+            await chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+            await chrome.action.setBadgeText({ tabId: tab[0].id, text: text });
+        }
+    });
+});
+
+// タブ更新時にバッジ設定
+// TODO: App.vueの部分といい感じに統合したい
+chrome.tabs.onUpdated.addListener(async function (_tabId, _changeInfo, tab) {
+    let text = "";
+    if (tab.id != undefined) {
+        await chrome.alarms.get(tab.id.toString(), function (alarm) {
+            if (alarm != undefined) {
+                text = typeof alarm.periodInMinutes === "number" ? alarm.periodInMinutes.toString() : ""
+            }
+        });
+        // TODO: 共通化したい
+        await chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+        await chrome.action.setBadgeText({ tabId: tab.id, text: text });
+    }
+});
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {crx, defineManifest} from '@crxjs/vite-plugin'
+// import manifest from './manifest.json' assert {type: 'json'} // Node >=17
+
+const manifest = defineManifest({
+    manifest_version: 3,
+    name: 'tab reloader',
+    description: 'Reload tab in periodic time',
+    version: '1.0.0',
+    permissions: [
+        "alarms",
+        "tabs",
+    ],
+    icons: {
+        '16': 'icons/icon_16.png',
+        '32': 'icons/icon_32.png',
+        '48': 'icons/icon_48.png',
+        '128': 'icons/icon_128.png',
+        '256': 'icons/icon_256.png',
+    },
+    action: {
+        'default_popup': 'index.html'
+    },
+    background: {
+        service_worker: 'src/background.ts',
+        type: "module",
+    }
+})
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        crx({manifest}),
+    ],
+})
